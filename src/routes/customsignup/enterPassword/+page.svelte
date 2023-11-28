657466055aee6f3260f22220f822121f2c580827
Jeffrey Kang <knightdips@gmail.com>
2023-11-27 20:47:05 -0800

    some stuff

---
+++ src/routes/customsignup/enterPassword/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+    import { myStore } from '../stores.js';
+
+    let password = '';
+
+    let emailValue = $myStore;
+    
+
+    
+</script>
+
+<form  method = "POST" action="?/register">
+    <input type="password" name = "password" placeholder="Password" required>
+    <input type="hidden" name="email" value= {emailValue}>
+    <button type="submit">Register</button>
+  </form>
